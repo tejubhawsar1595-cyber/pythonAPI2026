97e4b8c4dde475dc67279a9c37a605694e0f33d0
tejasbhawsar <teju.bhawsar1595@gmail.com>
2026-04-25 15:30:47 +0530

    made changes

--- test_singleBook_by_Id.py
+++ test_singleBook_by_Id.py
@@ -30,7 +30,7 @@ class TestGetSingleBook:
 
 
     @pytest.mark.parametrize("book_list",[1,2,3,4,5])
-    def test_multiple_book_ids(self,book_list):
+    def test_multiple_book_ids_line(self,book_list):
         response = requests.get(f"{base_url}/books/{book_list}")
         assert response.status_code in [200,404]
 
